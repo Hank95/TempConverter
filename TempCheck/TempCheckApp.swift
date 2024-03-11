f2b31e56fe89ebe466784d2b43a6d1994ee68e10
Henry Pendleton <hhpendleton@gmail.com>
2024-03-11 09:51:42 -0400

    Initial Commit

---
+++ TempCheck/TempCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempCheckApp.swift
+//  TempCheck
+//
+//  Created by Henry Pendleton on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TempCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
